f930cc4b44fa62285ec7d1857112d015f043e878
DESKTOP-POK99E7\Admin <khanhpqworking@gmail.com>
2025-09-10 21:30:18 +0700

    Interview prep, starting fresh!!!

---
+++ ForReviewing/Threading.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ForReviewing
+{
+	internal class Threading
+	{
+	}
+
+	class Account
+	{
+		public int Balance { get; private set; }
+		private readonly object lockObj = new object();
+
+		public Account(int balance) => Balance = balance;
+
+		public void Withdraw(int amount)
+		{
+			Balance -= amount;
+		}
+
+		public void Deposit(int amount)
+		{
+			Balance += amount;
+		}
+
+		public object GetLock() => lockObj;
+	}
+
+	class Program
+	{
+		// Dead lock
+		static void TransferDeadLock(Account from, Account to, int amount)
+		{
+			lock (from.GetLock())
+			{
+				Thread.Sleep(100); // long time
+				lock (to.GetLock())
+				{
+					from.Withdraw(amount);
+					to.Deposit(amount);
+					Console.WriteLine($"Chuyển {amount} từ {from.GetHashCode()} sang {to.GetHashCode()}");
+				}
+			}
+		}
+
+		static void TransferSafe(Account from, Account to, int amount)
+		{
+			Account first = from.GetHashCode() < to.GetHashCode() ? from : to;
+			Account second = from == first ? to : from;
+
+			lock (first.GetLock())
+			{
+				Thread.Sleep(100);
+				lock (second.GetLock())
+				{
+					from.Withdraw(amount);
+					to.Deposit(amount);
+					Console.WriteLine($"Move {amount} from {from.GetHashCode()} to {to.GetHashCode()}");
+				}
+			}
+		}
+
+		static void Main()
+		{
+			var acc1 = new Account(1000);
+			var acc2 = new Account(2000);
+
+			Thread t1 = new Thread(() => TransferSafe(acc1, acc2, 100));
+			Thread t2 = new Thread(() => TransferSafe(acc2, acc1, 200));
+
+			t1.Start();
+			t2.Start();
+
+			t1.Join();
+			t2.Join();
+
+			Console.WriteLine($"acc1: {acc1.Balance}, acc2: {acc2.Balance}");
+		}
+	}
+}
---
+++ LeetCodeCplusplus/LongestPalindromicSubstring.cpp
@@ -0,0 +1,39 @@
+/*Given a string s, return the longest palindromic substring in s.
+
+Example 1:
+
+Input: s = "babad"
+Output : "bab"
+Explanation : "aba" is also a valid answer.
+Example 2 :
+
+	Input : s = "cbbd"
+	Output : "bb"
+
+Constraints :
+
+	1 <= s.length <= 1000
+	s consist of only digits and English letters.*/
+
+#include <string>
+using namespace std;
+
+class Solution
+{
+public:
+	string longestPalindrome(string s)
+	{
+		string strLongestPalindrome = "";
+		int length = 0;
+		for (int i = 0; i < s.length(); i++)
+		{
+			
+		}
+	}
+};
+
+int main()
+{
+
+	return 0;
+}
